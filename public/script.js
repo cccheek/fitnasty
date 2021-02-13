2c0946db3ab7f39abaad6748bf51b5434665ac80
Claire Cheek <claireccheek@gmail.com>
2021-02-12 23:42:32 -0800

    almost there

--- public/script.js
+++ public/script.js
@@ -1,34 +1,46 @@
-function renderExercise(){
-    $("#week").empty();
-    $where.ajax({
+function renderExercise() {
+    $("#workouts").empty();
+    $.ajax({
         url: "/populateexercise",
-        method:"GET",
+        method: "GET",
     })
-    .then(dbData =>{
-        console.log(dbData)
-        dbData.forEach(plan => {
-
-            //make a new div each workout
-            const newDiv = $("<div>", {
-                style:"width 25%; border: 2px solid black",
-            })
-            const title = $("<h3>", {
-                text: plan.name
-            })
-            const newUl = $("<ul>", {text: "Exercise Done:"})
-            newDiv.append(title)
-
-
-            //loop through exercise and print each
-            plan.meal.forEach(exercise => {
-                const newLi = $("<li>", {
-                    text: `Name ${exercise.name}\nKind: ${exercise.kind}\nWeight: ${exercise.weight}\nReps: ${exercise.reps}\nSets: ${exercise.sets}\nCardio: ${exercise.cardio ? "Yes":"No"}\nDuration: ${exercise.duration}`
+        .then(dbData => {
+            // console.log(dbData)
+            dbData.forEach(plan => {
+                console.log(plan)
+                //make a new div each workout
+                const newDiv = $("<div>", {
+                    style: "width 25%; border: 2px solid black",
                 })
-                newUl.append(newLi);
+                const title = $("<h3>", {
+                    text: plan.name
+                })
+                const newUl = $("<ul>", { text: "Exercise!" })
+                newDiv.append(title)
+
+
+                //loop through exercise and print each
+                plan.exercise.forEach(exercise => {
+                    console.log(exercise)
+                    const newLi = $("<li>", {
+                        text: `Name ${exercise.name}\nKind: ${exercise.kind}\nWeight: ${exercise.weight}\nReps: ${exercise.reps}\nSets: ${exercise.sets}\nCardio: ${exercise.cardio ? "Yes" : "No"}\nDuration: ${exercise.duration}`
+                    })
+                    newUl.append(newLi);
+                })
+                newDiv.append(newUl);
+                $("#workouts").append(newDiv);
             })
-            newDiv.append(newUl);
-            $("#weeks").append(newDiv);
         })
-    })
 }
 renderExercise();
+
+$("#new-workout").on("submit", (event) => {
+    event.preventDefault();
+    const workoutDay = $("#workout-day").val().trim();
+    // console.log(workoutDay + "working")
+    $.ajax({
+        url: "/api/workouts",
+        method: "POST",
+        data: { name: workoutDay }
+    }).then(renderExercise())
+})
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 //requiring packages
-const compression = require("compression");
-const logger = require("morgan");
 const express = require("express");
 const mongoose = require("mongoose");
+const compression = require("compression");
+const logger = require("morgan");
 
 
 const app = express();
@@ -30,8 +30,76 @@ useCreateIndex: true
 //starter route
 app.get("/", (req, res) => {
     // res.send("oh heyyy");
-    res.send('./index.html');
+    res.sendFile('./index.html');
 })
+
+//seed data
+const seedWorkout =[
+    {
+        name: "yoga",
+        kind: "stretch",
+        weight: 0,
+        reps: 0,
+        sets: 0,
+        cardio: false,
+        duration: 30
+    },
+    {
+        name: "running",
+        kind: "outside",
+        weight: 0,
+        reps: 0,
+        sets: 0,
+        cardio: true,
+        duration: 30
+    },
+    {
+        name: "bicep curls",
+        kind: "easy",
+        weight: 15,
+        reps: 10,
+        sets: 4,
+        cardio: false,
+        duration: 30
+    }
+   
+]
+app.get("/seed", (req, res) => {
+    db.Exercise.create(seedWorkout)
+    .then(result => {
+        console.log(result)
+        db.Workout.create([
+            {
+                name: "workout 1",
+                exercises: [
+                    result[Math.floor(Math.random()* result.length)]._id
+                ]
+            },
+            {
+                name: "workout 2",
+                exercises: [
+                    result[Math.floor(Math.random()* result.length)]._id
+                ]
+            },
+            {
+                name: "workout 3",
+                exercises: [
+                    result[Math.floor(Math.random()* result.length)]._id
+                ]
+            },
+        ])
+        .then(fullRes => {
+            res.send(fullRes)
+        })
+        .catch(err => {
+            res.send(err)
+        })
+    })
+    .catch(err => {
+        res.send(err)
+    })
+})
+
 
 
 app.get('/api/exercise', (req, res) => {
@@ -58,7 +126,7 @@ app.get('/api/workout', (req, res) => {
 
 app.get('/populateexercise', (req, res) => {
     db.Workout.find({})
-    .populate('exercises')
+    .populate('exercise')
     .then(dbWorkout => {
         res.json(dbWorkout)
     })
@@ -68,6 +136,17 @@ app.get('/populateexercise', (req, res) => {
     })
 })
 
+
+app.post("/api/workouts", ({ body }, res) => {
+db.Workout.create(body)
+.then(dbWorkout => {
+    res.json(dbWorkout)
+})
+.catch(err =>{
+    console.log(err)
+    res.send(err)
+})
+})
 
 
 
